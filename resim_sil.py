6d329b1e9efcf8c59c481dc585c666544132343a
Sefa Bulak <zckhtln@icloud.com>
2024-03-05 23:55:20 +0100

    Update resim_sil.py

--- resim_sil.py
+++ resim_sil.py
@@ -21,13 +21,21 @@ else:
 
         dosya_adlari = [sayfa.cell(row=i, column=2).value for i in range(2, sayfa.max_row + 1) if sayfa.cell(row=i, column=2).value is not None]
 
+        # Geri dönüşüm klasörü yolu
+        geri_donusum_klasoru = os.path.join(klasor_yolu, "Geri Dönüşüm")
+        if not os.path.exists(geri_donusum_klasoru):
+            os.makedirs(geri_donusum_klasoru)
+
         klasordeki_dosyalar = os.listdir(klasor_yolu)
 
         for dosya_adi in klasordeki_dosyalar:
-            if dosya_adi not in dosya_adlari:
-                dosya_yolu = os.path.join(klasor_yolu, dosya_adi)
-                if os.path.exists(dosya_yolu):
-                    os.remove(dosya_yolu)
-                    print(f"{dosya_adi} onaylanmadı.")
+            kaynak_dosya_yolu = os.path.join(klasor_yolu, dosya_adi)
+            hedef_dosya_yolu = os.path.join(geri_donusum_klasoru, dosya_adi)
+            if dosya_adi not in dosya_adlari and os.path.isfile(kaynak_dosya_yolu):  # Ek dosya kontrolü
+                try:
+                    os.rename(kaynak_dosya_yolu, hedef_dosya_yolu)
+                    print(f"{dosya_adi} geri dönüşüm klasörüne taşındı.")
+                except OSError as e:
+                    print(f"{dosya_adi} taşınamadı: {e}")
 
         excel_dosyasi.close()
